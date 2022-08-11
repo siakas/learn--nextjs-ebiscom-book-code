0eb64403096460098c580d604ca4f45831af5b2f
Sanshiroh SAKAI <sansyo@gmail.com>
2022-08-11 22:34:34 +0900

    基本の記述を学習

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <header>Header</header>
+      <hr />
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+
+      <hr />
+      <footer>Footer</footer>
+    </>
+  )
+}
